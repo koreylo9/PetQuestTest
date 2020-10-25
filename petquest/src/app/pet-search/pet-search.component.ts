bb2054634dcadd3c61e856a47fb593ad3d0cfdb4
Korey Lo <koreylo9@gmail.com>
2020-10-25 16:36:06 -0700

    Dummy array that stores dogs and display to dom

--- petquest/src/app/pet-search/pet-search.component.ts
+++ petquest/src/app/pet-search/pet-search.component.ts
@@ -19,6 +19,12 @@ export class PetSearchComponent implements OnInit {
 
   model = new PetInfo();
 
+  Dogs = [{name: 'Pet1', breed: 'Poodle', age: '8 months', gender: 'Male', location: 'San Diego, CA'},
+  {name: 'Pet2', breed: 'Husky', age: '6 months', gender: 'Male', location: 'San Francisco, CA'},
+  {name: 'Pet3', breed: 'Cocker Spaniel', age: '5 months', gender: 'Male', location: 'Merced, CA'},
+  {name: 'Pet4', breed: 'Terrier', age: '6 months', gender: 'Female', location: 'Stockton, CA'},
+  {name: 'Pet5', breed: 'Pitbull', age: '11 months', gender: 'Female', location: 'Fresno, CA'}];
+
   Breeds: string[] = [
     'Golden Retriever',
     'Husky',
