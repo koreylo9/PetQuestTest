a37b7c21916e270c3c16a3a9235f6cb269e2725a
jingyu120 <justin.zhang@tmps-nb-2038.local>
2020-10-25 16:07:54 -0500

    pet care page transferred

--- petquest/src/app/app-routing.module.ts
+++ petquest/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { PetManagementComponent } from './pet-management/pet-management.component';
 import { PetManagementAddComponent } from './pet-management-add/pet-management-add.component';
 import { PetManagementViewComponent } from './pet-management-view/pet-management-view.component';
+import {PetCareComponent} from './pet-care/pet-care.component';
 
 const routes: Routes = [
   { path: 'home-page', component: HomePageComponent},
   { path: 'pet-management', component: PetManagementComponent},
   { path: 'pet-management-add', component: PetManagementAddComponent},
   { path: 'pet-management-view', component: PetManagementViewComponent},
+  { path: 'pet-care', component: PetCareComponent},
   { path: '', redirectTo: 'home-page', pathMatch: 'full'}
 ];
 
--- petquest/src/app/app.module.ts
+++ petquest/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { PetManagementComponent } from './pet-management/pet-management.componen
 import { PetManagementAddComponent } from './pet-management-add/pet-management-add.component';
 import { PetManagementViewComponent } from './pet-management-view/pet-management-view.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PetCareComponent } from './pet-care/pet-care.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
     HomePageComponent,
     PetManagementComponent,
     PetManagementAddComponent,
-    PetManagementViewComponent
+    PetManagementViewComponent,
+    PetCareComponent
   ],
   imports: [
     BrowserModule,
